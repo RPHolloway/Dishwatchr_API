36923aca8b244efcdb4b6ddfad04be2567922e29
rholloway <rick@tri-ware.com>
2017-09-24 16:54:17 -0400

    Remove example code

--- server.js
+++ server.js
@@ -3,15 +3,6 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080
 
-// routes eventually go here
-app.get('/api/users', function(req, res) {
-    var user_id = req.param('id');
-    var token = req.param('token');
-    var geo = req.param('geo');
-
-    res.send(user_id + ' ' + token + ' ' + geo);
-});
-
 //-- Create Dishwasher --//
 //      Dishwasher:
 //      {
