1123102ad0086edfa4bfad8f14c1f6cea904db5b
rholloway <rick@tri-ware.com>
2017-09-24 16:45:48 -0400

    Stub out structs and API calls

--- server.js
+++ server.js
@@ -12,21 +12,63 @@ app.get('/api/users', function(req, res) {
     res.send(user_id + ' ' + token + ' ' + geo);
 });
 
-// Get new dishwasher id
-// Get status of dishwasher
-// Set status of dishwasher
+//-- Create Dishwasher --//
+//      Dishwasher:
+//      {
+//          id,
+//          state,
+//          model,
+//          estimated wash time,
+//      }
 
-// Get status of dishwasher pseudocode
-// 1. Open app
-// 2. Check for stored dishwasher ID
-// 3. If no dishwasher ID
+//-- Get Dishwasher --//
+//-- Update Dishwasher Info --//
+//-- Toggle Dishwasher State --//
+
+//-- Create User --//
+//      User:
+//      {
+//          id,
+//          username,
+//          password
+//          first name,
+//          last name,
+//      }
+
+//-- Get User --//
+//-- Update User Info --//
+
+//-- Create House --//
+//      House:
+//      {
+//          id,
+//          name,
+//          address,
+//          password (optional),
+//          dishwashers[],
+//          users[],
+//      }
+
+//-- Get House --//
+//-- Update House Info --//
+//-- Add user to House --//
+//-- Add Dishwasher to House --//
+
+//*** 'Get Status of Dishwasher' ***//
+// 1. Open app.
+// 2. Check house membership status
+//      Member of a house?
+//          Yes - Load main menu.
+//          No - Do you want to create a new house?
+//              Yes - Create a new house w/ info.
+//              No - Can't progress in app. Wait for user to decide 'yes'  .      
+// 3. Check for stored dishwasher ID.
+// 4. If no dishwasher ID.
 //      Do you want to create a new dishwasher ID? 
-//          Yes - Create new dishwasher ID from server
-//          No - type in dishwasher ID, store locally
-// 4. Using new dishwasher ID or stored old dishwasher ID,
-//    run GetStatusOfDishwasher from server, passing dishwasher
-//    ID as parameter in function call
-// 5. Display status
+//          Yes - Create new dishwasher ID from server.
+//          No - Main Menu will be empty :(
+// 5. Get Dishwasher by ID from server.
+// 6. Display status
 
 // start the node server
 app.listen(port); 
