ed8bb4486f9cdc2b302477ce86d62dfacaf94c0f
rholloway <rick@tri-ware.com>
2017-09-25 19:58:15 -0400

    Add Basic tables    - dishwasher    - house    - user    - house_user_bridge Add node scripts to drop and create

---
+++ dishwasher.js
@@ -0,0 +1,32 @@
+var AWS = require("aws-sdk")
+
+AWS.config.update({
+    accessKeyId: "AKIAIH3HQXDW4QUB25YQ",
+    secretAccessKey: "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", 
+    region: "us-west-2",
+    endpoint: "http://localhost:8000"
+});
+
+var dynamodb = new AWS.DynamoDB();
+
+var params = {
+    TableName: "Dishwashers",
+    KeySchema: [
+        {AttributeName: "id", KeyType: "HASH"}
+    ],
+    AttributeDefinitions: [
+        {AttributeName: "id", AttributeType: "S"}
+    ],
+    ProvisionedThroughput: {
+        ReadCapacityUnits: 10,
+        WriteCapacityUnits: 10
+    }
+}
+
+dynamodb.createTable(params, function(err, data) {
+    if (err) {
+        console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+    }
+})
---
+++ house_user_bridge.js
@@ -0,0 +1,34 @@
+var AWS = require("aws-sdk")
+
+AWS.config.update({
+    accessKeyId: "AKIAIH3HQXDW4QUB25YQ",
+    secretAccessKey: "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", 
+    region: "us-west-2",
+    endpoint: "http://localhost:8000"
+});
+
+var dynamodb = new AWS.DynamoDB();
+
+var params = {
+    TableName: "House_User_BRIDGE",
+    KeySchema: [
+        {AttributeName: "user_id", KeyType: "HASH"},
+        {AttributeName: "username", KeyType: "RANGE"}
+    ],
+    AttributeDefinitions: [
+        {AttributeName: "user_id", AttributeType: "S"},
+        {AttributeName: "username", AttributeType: "S"}
+    ],
+    ProvisionedThroughput: {
+        ReadCapacityUnits: 10,
+        WriteCapacityUnits: 10
+    }
+}
+
+dynamodb.createTable(params, function(err, data) {
+    if (err) {
+        console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+    }
+})
