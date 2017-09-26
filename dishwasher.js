4b3d9f834cf1f5b58326991e7e1a948aadcff071
rholloway <rick@tri-ware.com>
2017-09-25 20:03:04 -0400

    secret

--- dishwasher.js
+++ dishwasher.js
@@ -1,8 +1,6 @@
 var AWS = require("aws-sdk")
 
 AWS.config.update({
-    accessKeyId: "AKIAIH3HQXDW4QUB25YQ",
-    secretAccessKey: "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", 
     region: "us-west-2",
     endpoint: "http://localhost:8000"
 });
