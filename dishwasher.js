c9fdc85d9a2b8fc536d414c8dc85904802b46f0f
rholloway <rick@tri-ware.com>
2017-09-25 20:29:14 -0400

    Add drop tables to clean

--- dishwasher.js
+++ dishwasher.js
@@ -7,24 +7,43 @@ AWS.config.update({
 
 var dynamodb = new AWS.DynamoDB();
 
-var params = {
-    TableName: "Dishwashers",
-    KeySchema: [
-        {AttributeName: "id", KeyType: "HASH"}
-    ],
-    AttributeDefinitions: [
-        {AttributeName: "id", AttributeType: "S"}
-    ],
-    ProvisionedThroughput: {
-        ReadCapacityUnits: 10,
-        WriteCapacityUnits: 10
+drop()
+create()
+
+function drop() {
+    var params = {
+        TableName: "Dishwashers"
     }
+
+    dynamodb.deleteTable(params, function(err, data) {
+        if (err) {
+            console.error("Unable to delete table. Error JSON:", JSON.stringify(err, null, 2))
+        } else {
+            console.log("Deleted table. Table description JSON:", JSON.stringify(data, null, 2))
+        }
+    })
 }
 
-dynamodb.createTable(params, function(err, data) {
-    if (err) {
-        console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
-    } else {
-        console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+function create() {
+    var params = {
+        TableName: "Dishwashers",
+        KeySchema: [
+            {AttributeName: "id", KeyType: "HASH"},
+        ],
+        AttributeDefinitions: [
+            {AttributeName: "id", AttributeType: "S"},
+        ],
+        ProvisionedThroughput: {
+            ReadCapacityUnits: 10,
+            WriteCapacityUnits: 10
+        }
     }
-})
+
+    dynamodb.createTable(params, function(err, data) {
+        if (err) {
+            console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2))
+        } else {
+            console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2))
+        }
+    })
+}
--- user.js
+++ user.js
@@ -7,26 +7,45 @@ AWS.config.update({
 
 var dynamodb = new AWS.DynamoDB();
 
-var params = {
-    TableName: "Users",
-    KeySchema: [
-        {AttributeName: "id", KeyType: "HASH"},
-        {AttributeName: "username", KeyType: "RANGE"}
-    ],
-    AttributeDefinitions: [
-        {AttributeName: "id", AttributeType: "S"},
-        {AttributeName: "username", AttributeType: "S"}
-    ],
-    ProvisionedThroughput: {
-        ReadCapacityUnits: 10,
-        WriteCapacityUnits: 10
+drop()
+create()
+
+function drop() {
+    var params = {
+        TableName: "Users"
     }
+
+    dynamodb.deleteTable(params, function(err, data) {
+        if (err) {
+            console.error("Unable to delete table. Error JSON:", JSON.stringify(err, null, 2))
+        } else {
+            console.log("Deleted table. Table description JSON:", JSON.stringify(data, null, 2))
+        }
+    })
 }
 
-dynamodb.createTable(params, function(err, data) {
-    if (err) {
-        console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
-    } else {
-        console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+function create() {
+    var params = {
+        TableName: "Users",
+        KeySchema: [
+            {AttributeName: "id", KeyType: "HASH"},
+            {AttributeName: "username", KeyType: "RANGE"}
+        ],
+        AttributeDefinitions: [
+            {AttributeName: "id", AttributeType: "S"},
+            {AttributeName: "username", AttributeType: "S"}
+        ],
+        ProvisionedThroughput: {
+            ReadCapacityUnits: 10,
+            WriteCapacityUnits: 10
+        }
     }
-})
+
+    dynamodb.createTable(params, function(err, data) {
+        if (err) {
+            console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2))
+        } else {
+            console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2))
+        }
+    })
+}
